b466b607ace99a5d6b130b3d6bc0b37b047190ea
三ッ國拓真 <lupin.create@hotmail.co.jp>
2014-06-21 19:32:41 +0900

    no message

--- fictitious-play.py
+++ fictitious-play.py
@@ -1,19 +1,32 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from random import uniform, randint
-from random import normalvariate
+from random import uniform
 
-#profits = 1,−1−1,1−1,1 1,−1
 
-#bilief(
-alpha = 0.9
-ts_length = 200
-current_x = 0
 
-x_values = []
-for t in range(ts_length):
-    x_values.append(current_x)
-    a = uniform(0, 1)
-    current_x = current_x + (a - current_x)/(t+2)  
-plt.plot(x_values, 'b-')
+times = 100  # 試行回数
+p = [[1,-1], [-1, 1], [-1,1], [1, -1]] #利得
 
+
+x0 = [uniform(0, 1)] #初期信念をランダムに設定,appendを使い後に更新する
+x1 = [uniform(0, 1)]
+
+
+for t in range(times):
+   ep0 = [p[0][0]*(1-x0[t])+p[1][0]*x0[t], p[2][0]*(1-x0[t])+p[3][0]*x0[t]]#期待利得
+   ep1 = [p[0][1]*(1-x1[t])+p[2][1]*x1[t], p[1][1]*(1-x1[t])+p[3][1]*x1[t]]
+   if ep0[0] > ep0[1]:
+        a0 = 0
+   else:
+        a0 = 1
+    
+   if ep1[0] > ep1[1]:
+        a1 = 0
+   else:
+        a1 = 1
+   x0.append(x0[t]+(a1-x0[t])/(t+2))
+   x1.append(x1[t]+(a0-x1[t])/(t+2))
+
+plt.plot(x0, 'r-', label = 'x0(t)')
+plt.plot(x1, 'b-', label = 'x1(t)')
 plt.show()
